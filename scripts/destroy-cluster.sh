f58ff0ad9c31b498134911a1c21a714175dde468
Juan Enciso <juan.enciso@gmail.com>
2024-08-03 22:22:55 +0100

    [FEAT] Add recomendation fror shellcheck

--- scripts/destroy-cluster.sh
+++ scripts/destroy-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kind delete cluster --name ${CLUSTER_NAME}
-for DOCKER_ID in $(docker ps -a --no-trunc --filter name=^${CLUSTER_NAME}- -q); do
- docker rm -f $DOCKER_ID
+kind delete cluster --name "${CLUSTER_NAME}"
+for DOCKER_ID in $(docker ps -a --no-trunc --filter name="^${CLUSTER_NAME}-" -q); do
+ docker rm -f "$DOCKER_ID"
 done
-docker network rm ${CLUSTER_NAME}
+docker network rm "${CLUSTER_NAME}"
